a234d460e9bcfc4bbab6465965d3e3510492302e
dmitriy_zavadskoy <dmitryi.zavadskyi@mail.ru>
2018-09-21 21:07:52 +0300

    добавление комментов

--- app/Services/Channels/GroupsService.php
+++ app/Services/Channels/GroupsService.php
@@ -7,27 +7,54 @@ use App\Http\Requests\Channels\GroupRequest;
 use App\Models\Channels\Group;
 use App\Repositories\Channels\GroupsRepository;
 
+/**
+ * Service for manage channels groups
+ */
 class GroupsService
 {
+    /**
+     * @var GroupsRepository
+     */
     protected $repository;
 
+    /**
+     * Construct for Group service
+     *
+     * @param GroupsRepository $repository
+     */
     public function __construct(GroupsRepository $repository)
     {
         $this->repository = $repository;
     }
 
+    /**
+     * Method for create group
+     *
+     * @param GroupRequest $request
+     * @return Group
+     */
     public function create(GroupRequest $request): Group
     {
         return $this->repository->create($request);
     }
 
-
+    /**
+     * Method for update group
+     *
+     * @param GroupRequest $request
+     * @param Group $group
+     * @return Group
+     */
     public function update(GroupRequest $request, Group $group): Group
     {
-
+        return $this->repository->update($request, $group);
     }
 
-
+    /**
+     * Method for destroy group
+     *
+     * @param GroupRequest $request
+     */
     public function destroy(GroupRequest $request)
     {
 
