d15149f9af99b1f3c5fd806ecdd91a3ed38bce99
skat <kirill712@ukr.net>
2020-02-11 15:50:56 +0300

    git integration test

--- app/Http/Controllers/Hooks/HooksController.php
+++ app/Http/Controllers/Hooks/HooksController.php
@@ -5,19 +5,25 @@ namespace App\Http\Controllers\Hooks;
 use App\Http\Controllers\Controller;
 use App\Integrations\IntegrationHandlerFactory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class HooksController extends Controller
 {
     public function handle(Request $request,$type,$id)
     {
-        $handler = IntegrationHandlerFactory::createHandler($type,$id);
+        try {
+            $handler = IntegrationHandlerFactory::createHandler($type,$id);
 
-        $confirmation = $handler->confirmServer($request);
+            $confirmation = $handler->confirmServer($request);
 
-        if($confirmation !== false){
-            return $confirmation;
+            if($confirmation !== false){
+                return $confirmation;
+            }
+
+            return $handler->acceptHook($request);
+
+        }catch (\Exception $e){
+            Log::error($e->getMessage().' '. $e->getTraceAsString());
         }
-
-        return $handler->acceptHook($request);
     }
 }
